b987806aba747b7b8fa18ed72246fef061a4b3ad
Phirasin Puisto <asin.puisto@gmail.com>
2026-09-08 17:12:51 +0300

    peliprojekti eri funktioita

--- peliprojekti1/peliprojekti.py
+++ peliprojekti1/peliprojekti.py
@@ -1,9 +1,38 @@
+import random
 
 nimi=input("Anna pelaajan nimi: ")
 
 ikä=int(input("Anna pelaajan ikä: "))
 
 print("nimi: " , nimi  , "ikä: " , ikä)
+
+esineet=[]
+
+def kysy_esine():
+        kysymys = input ("anna pelaajalle esine tai lopeta painamalla enter: ")
+        esineet.append(kysymys)
+        print("pelaaja on saanut esineen", kysymys)
+        while kysymys != "":
+            kysymys = input("anna pelaajalle uusi esine tai lopeta painamalla enter: ")
+            esineet.append(kysymys)
+            print("pelaaja on saanut esineen", kysymys)
+        
+def gifting():
+        giftrandom=(random.randint(1,2))
+        if giftrandom == 1:
+             print("olet saanut lahjasta hatun")
+             esineet.append("hattu")
+        if giftrandom == 2:
+             print("olet saanut lahjasta lemmikki lohikäärmeen")
+             esineet.append("lohikäärme miku")
+
+     
+
+def listan_sisalto():
+     print("näytetään pelaajalle tavaraluettelo")
+     print(esineet)
+
+
 
 if ikä < 12:
     print("pelaaja on alaikäinen")
@@ -12,17 +41,18 @@ if ikä >= 12:
         print("hei!", nimi)
         while True:     
             print("päävalikko!!")
-            print("kirjoita joko " "tarina"", ""lahja"", ""pakene"" tai lopeta")
+            print("kirjoita joko " "esine"", ""tavaraluettelo"", ""lahja"" tai lopeta")
             komento=input("Anna komento: ")
 
-            if komento == "tarina":
-                print("hei!", nimi , "tänään on kiva ja aurinkoinen päivä helsingissä toivottavasti sielläkin on aurinkoista")
+            if komento == "esine":
+                kysy_esine()
+                
 
             if komento == "lahja":
-                print(nimi , "!", "tässä sinulle uusi auto")
+                gifting()
 
-            if komento == "pakene":
-                 print("pelaaja", nimi ,"on valinnut juosta pakoon")
+            if komento == "tavaraluettelo":
+                 listan_sisalto()
 
             if komento == "lopeta": 
                 break
